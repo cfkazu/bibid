19412b66a6f0932bc87e8e5c85c9b2f646e34de3
CFkazu <kkkazu@g.ecc.u-tokyo.ac.jp>
2022-11-12 23:49:36 +0900

    add ALLOWED_HOSTS

--- bibid/settings.py
+++ bibid/settings.py
@@ -28,7 +28,7 @@ DEBUG = env('DEBUG')
 # SECURITY WARNING: keep the secret key used in production secret!
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 
 # Application definition
@@ -115,9 +115,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
