a5b2b760e2dbaa972c96f493b7c89e948a9e462a
CFkazu <kkkazu@g.ecc.u-tokyo.ac.jp>
2022-11-21 05:41:37 +0900

    blob名前被り問題解決

--- server/views.py
+++ server/views.py
@@ -345,6 +345,7 @@ class ImageCreate(generics.CreateAPIView):
         if request.data['decription'] == "undefined":
             request.data['decription'] = ""
 
+        request.data['image']._name = request.data['image'].name+str(ImageModel.objects.all().count())
         newimg = ImageModel(title=request.data['title'], image=request.data['image'],
                             prompt=request.data['prompt'], neg_prompt=request.data['neg_prompt'], additonal_tags=request.data['additonal_tags'],
                             decription=request.data['decription'], good=request.data['good'], is_nsfw=request.data['is_nsfw'], seed=request.data['seed'],
