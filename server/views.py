2a07acc081f1e706a777f5a83116c24a6a11fae4
CFkazu <kkkazu@g.ecc.u-tokyo.ac.jp>
2022-11-22 23:38:17 +0900

    プロフ画像高画質化

--- server/views.py
+++ server/views.py
@@ -553,8 +553,7 @@ def twitter_callback(request):
             # Create user
             info = twitter_api.get_me(access_token, access_token_secret)
             if info is not None:
-                print(info[0]['id'])
-                print(info[0]['profile_image_url'])
+                info[0]['profile_image_url'] = info[0]['profile_image_url'].replace('_normal', '_400x400')
                 twitter_user_new = TwitterUser(twitter_id=info[0]['id'], screen_name=info[0]['username'],
                                                name=info[0]['name'], profile_image_url=info[0]['profile_image_url'])
                 twitter_user_new.twitter_oauth_token = twitter_auth_token
